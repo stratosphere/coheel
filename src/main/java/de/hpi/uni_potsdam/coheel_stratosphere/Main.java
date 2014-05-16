fd2f926d0948ffbbabdb94cd45821fae6aa0fb82
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-16 19:18:54 +0200

    Refactored link extracting.

--- src/main/java/de/hpi/uni_potsdam/coheel_stratosphere/Main.java
+++ src/main/java/de/hpi/uni_potsdam/coheel_stratosphere/Main.java
@@ -3,10 +3,7 @@ package de.hpi.uni_potsdam.coheel_stratosphere;
 import org.dbpedia.extraction.sources.WikiPage;
 import org.dbpedia.extraction.util.Language;
 import org.dbpedia.extraction.util.WikiApi;
-import org.dbpedia.extraction.wikiparser.LinkNode;
-import org.dbpedia.extraction.wikiparser.Node;
-import org.dbpedia.extraction.wikiparser.PageNode;
-import org.dbpedia.extraction.wikiparser.WikiParser;
+import org.dbpedia.extraction.wikiparser.*;
 import org.dbpedia.extraction.wikiparser.impl.simple.SimpleWikiParser;
 import scala.collection.Iterable;
 import scala.collection.Iterator;
@@ -19,34 +16,28 @@ import java.util.List;
 
 public class Main {
 
-	public static void main(String[] args) throws MalformedURLException {
-		WikiApi wikiApi = new WikiApi(new URL("http://en.wikipedia.org/w/api.php"), Language.English());
-		List<Object> pageId = new ArrayList<>();
-//		pageId.add(41528);
-		pageId.add(4447008);
-		Iterable<Object> it = JavaConverters.asScalaIterableConverter(pageId).asScala();
-		WikiPage wikiPage = wikiApi.retrievePagesByPageID(it).toList().head();
-
-		WikiParser wikiParser = new SimpleWikiParser();
-		PageNode ast = wikiParser.apply(wikiPage);
-		findLinks(ast);
+	public static void main(String[] args) {
+//		WikiPage page = getExampleWikiPage(4447008); // Scintilla
+//		WikiPage page = getExampleWikiPage(41528); // Forrest Gump
+		WikiPage page = getExampleWikiPage(11867); // Germany
+		LinkExtractor linkExtractor = new LinkExtractor();
+		linkExtractor.extractLinks(page);
 	}
 
-	private static void findLinks(Node ast) {
-		Iterator<Node> it = ast.children().iterator();
-		while (it.hasNext()) {
-			Node node = it.next();
-			if (node instanceof LinkNode) {
-				LinkNode linkNode = (LinkNode) node;
-//				System.out.println(node.toPlainText());
-				System.out.println(linkNode.toWikiText());
-			}
-			Iterator<Node> nodeIt = node.children().iterator();
-			while (nodeIt.hasNext()) {
-				findLinks(nodeIt.next());
-			}
+	public static WikiPage getExampleWikiPage(int pageId) {
+		WikiApi wikiApi = null;
+		try {
+			wikiApi = new WikiApi(
+				new URL("http://en.wikipedia.org/w/api.php"),
+				Language.English());
+			List<Object> pageIds = new ArrayList<>();
+			pageIds.add(pageId);
+			Iterable<Object> it = JavaConverters.asScalaIterableConverter(pageIds).asScala();
+			WikiPage wikiPage = wikiApi.retrievePagesByPageID(it).toList().head();
+			return wikiPage;
+		} catch (MalformedURLException e) {
+			throw new RuntimeException(e);
 		}
-
-
 	}
+
 }
---
+++ src/main/test/de/hpi/uni_potsdam/coheel_stratosphere/LinkTest.java
@@ -0,0 +1,20 @@
+package de.hpi.uni_potsdam.coheel_stratosphere;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class LinkTest {
+
+	@Test
+	public void thisAlwaysPasses() {
+
+	}
+
+	@Test
+	@Ignore
+	public void thisIsIgnored() {
+	}
+}
