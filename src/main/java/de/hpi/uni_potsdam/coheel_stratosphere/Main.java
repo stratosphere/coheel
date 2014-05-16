e6bbe92a065332cde7a175e4a2b8f47c086f2acd
Stefan Bunk <stefan.bunk@googlemail.com>
2014-05-16 14:04:13 +0200

    Convert to maven project.

---
+++ src/main/java/de/hpi/uni_potsdam/coheel_stratosphere/Main.java
@@ -0,0 +1,13 @@
+package de.hpi.uni_potsdam.coheel_stratosphere;
+
+import org.dbpedia.extraction.wikiparser.WikiParser;
+import org.dbpedia.extraction.sources.WikiPage;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("TEST");
+		WikiParser wikiParser = new WikiParser();
+//		wikiParser.apply();
+	}
+}
