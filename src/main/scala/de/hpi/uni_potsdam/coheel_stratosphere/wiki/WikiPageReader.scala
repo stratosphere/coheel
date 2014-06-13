e5ad262d63a4a39e6f7adb712c2879756123fcad
Stefan Bunk <stefan.bunk@googlemail.com>
2014-06-13 11:46:17 +0200

    Adapted to new xml format.

--- src/main/scala/de/hpi/uni_potsdam/coheel_stratosphere/wiki/WikiPageReader.scala
+++ src/main/scala/de/hpi/uni_potsdam/coheel_stratosphere/wiki/WikiPageReader.scala
@@ -28,25 +28,23 @@ object WikiPageReader {
 			def hasNext = pages.hasNext
 			def next(): WikiPage = {
 				val page = pages.next()
-				val rev = page \  "revisions" \ "rev"
+				val rev = page \  "revision"
 
-				val contributorID = rev \ "@userid"
-				val contributorName = rev \ "@user"
-				val format = rev \ "@contentformat"
+				val contributorID = rev \ "contributor" \ "id"
+				val contributorName = rev \ "contributor" \ "username"
 
 				new WikiPage(
-					title           = WikiTitle.parse((page \ "@title").head.text, Language.English),
+					title           = WikiTitle.parse((page \ "title").head.text, Language.English),
 					redirect        = null,
-					id              = (page \ "@pageid").head.text,
-					revision        = (rev \ "@revid").head.text,
-					timestamp       = (rev \ "@timestamp").head.text,
+					id              = (page \ "id").head.text,
+					revision        = (rev \ "id").head.text,
+					timestamp       = (rev \ "timestamp").head.text,
 					contributorID   = if (contributorID == null || contributorID.length != 1)
 						"0" else contributorID.head.text,
 					contributorName = if (contributorName == null || contributorName.length != 1)
 						"" else contributorName.head.text,
-					source          = rev.text,
-					format          = if (format == null || format.length != 1)
-						"" else format.head.text
+					source          = (rev \ "text").text,
+					format          = "text/x-wiki"
 				)
 			}
 		}
