9649a9d1e6d6840612b3f0d4befc77ef4620e059
Stefan Bunk <stefan.bunk@googlemail.com>
2014-07-28 20:40:12 +0200

    Add script to make files gold standard.
    Former-commit-id: 470bb4800bb690014f4c7b6efc9d4a7f908f9e72

--- src/main/scala/de/uni_potsdam/hpi/coheel/plans/OutputFiles.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/plans/OutputFiles.scala
@@ -5,12 +5,12 @@ import eu.stratosphere.api.scala.operators.{CsvInputFormat, CsvOutputFormat}
 object OutputFiles {
 	lazy val currentPath = System.getProperty("user.dir")
 
-	lazy val surfaceProbsPath      = s"file://$currentPath/testoutput/surface-probs"
-	lazy val contextLinkProbsPath  = s"file://$currentPath/testoutput/context-link-probs"
-	lazy val languageModelsPath    = s"file://$currentPath/testoutput/language-models"
-	lazy val redirectPath          = s"file://$currentPath/testoutput/redirects"
-	lazy val surfaceDocumentPath   = s"file://$currentPath/testoutput/surface-document-counts"
-	lazy val linkOccurenceCounts   = s"file://$currentPath/testoutput/link-occurrence-counts"
+	lazy val surfaceProbsPath      = s"file://$currentPath/testoutput/surface-probs.wiki"
+	lazy val contextLinkProbsPath  = s"file://$currentPath/testoutput/context-link-probs.wiki"
+	lazy val languageModelsPath    = s"file://$currentPath/testoutput/language-models.wiki"
+	lazy val redirectPath          = s"file://$currentPath/testoutput/redirects.wiki"
+	lazy val surfaceDocumentPath   = s"file://$currentPath/testoutput/surface-document-counts.wiki"
+	lazy val linkOccurenceCounts   = s"file://$currentPath/testoutput/link-occurrence-counts.wiki"
 
 	val outputFormat          = CsvOutputFormat[(String, String, Int)]("\n", "\t")
 	val inputFormat           = CsvInputFormat[(String, String, Int)]("\n", '\t')
---
+++ testoutput/make-gold.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+rename 's/(.*)/$1.gold/' *.wiki
