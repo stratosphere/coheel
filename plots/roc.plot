15529a6b28a61603d3ed4783fe59e6b7038e3be1
Stefan Bunk <stefan.bunk@googlemail.com>
2014-08-14 12:55:11 +0200

    Started ROC plot.
    Former-commit-id: a20315b142f850ecbcd118bde770e157be09b384

---
+++ plots/roc.plot
@@ -0,0 +1,8 @@
+set terminal wxt size 800,700
+set xrange [0:1]
+set yrange [0:1]
+set key right bottom
+set datafile separator ","
+plot 'roc.csv' using 1:2 title 'ROC curve' pt 7, \
+	 x, \
+#     'roc.csv' using 1:2:3 with labels offset 3, 0, 5, 0 notitle, \
--- src/main/scala/de/uni_potsdam/hpi/coheel/Main.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/Main.scala
@@ -6,7 +6,7 @@ import com.typesafe.config.ConfigFactory
 import de.uni_potsdam.hpi.coheel.programs.{RedirectResolvingProgram, SurfaceNotALinkCountProgram, WikipediaTrainingProgram}
 import org.apache.commons.io.FileUtils
 import scala.collection.JavaConversions._
-import java.io.File
+import java.io._
 import eu.stratosphere.api.common.Program
 
 object Main {
@@ -27,6 +27,7 @@ object Main {
 //			new SurfaceNotALinkCountProgram
 //		    new RedirectResolvingProgram
 		runProgram(program)
+//		buildRocPlot()
 	}
 
 	def runProgram(program: Program): Unit = {
@@ -41,6 +42,39 @@ object Main {
 		} * 10.2 * 1024 /* full data dump size*/ / 42.7 /* test dump size */ / 60 /* in minutes */ / 60 /* in hours */
 		if (config.getBoolean("print_approximation"))
 			println(f"Approximately $processingTime%.2f hours on the full dump, one machine.")
+	}
+
+	def buildRocPlot(): Unit = {
+		val bw = new BufferedWriter(new FileWriter("plots/roc.csv", false))
+		(0 to 4400 by 20).foreach { threshold =>
+			var tp = 0
+			var fp = 0
+			var fn = 0
+			var tn = 0
+
+			val br = new BufferedReader(new FileReader("testoutput/possible-surface-occurrences.wiki"))
+			var line: String = br.readLine()
+			while (line != null) {
+				val split = line.split('\t')
+				val actual = split(2).toInt
+				val possible = split(3).toInt
+				if (possible > threshold) {
+					fn += actual
+					tn += (possible - actual)
+				} else {
+					tp += actual
+					fp += (possible - actual)
+				}
+				line = br.readLine()
+			}
+
+			val tpr = tp.toDouble / (tp + fn).toDouble
+			val fpr = fp.toDouble / (fp + tn).toDouble
+
+			bw.write(s"$fpr,$tpr,$threshold")
+			bw.newLine()
+		}
+		bw.close()
 	}
 
 	def time[R](block: => R): Double = {
