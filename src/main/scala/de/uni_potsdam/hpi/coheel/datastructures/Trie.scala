3566cc9d7d07b3e0bede32034335a12e581f254d
Stefan Bunk <stefan.bunk@googlemail.com>
2014-08-21 13:55:49 +0200

    Using trove map for the hash values.
    Former-commit-id: b0b32a88e90562df5345f3bb8e2751f2eaea07e9

--- src/main/scala/de/uni_potsdam/hpi/coheel/datastructures/Trie.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/datastructures/Trie.scala
@@ -3,6 +3,8 @@ package de.uni_potsdam.hpi.coheel.datastructures
 import java.util
 import java.util.Map
 
+import gnu.trove.map.hash.TIntObjectHashMap
+
 case class Trie() {
 
 	val rootNode = TrieNode()
@@ -23,28 +25,29 @@ case class Trie() {
 }
 
 case class TrieNode() {
+
 	var isEntry = false
 
-	var children: Map[String, TrieNode] = _
+	var children: TIntObjectHashMap[TrieNode] = _
 
 	def add(tokens: Seq[String]): Unit = {
 		if (children == null)
-			children = new util.HashMap()
+			children = new TIntObjectHashMap[TrieNode]()
 		if (tokens.tail.isEmpty) {
-			children.get(tokens.head) match {
+			children.get(tokens.head.hashCode) match {
 				case null =>
 					val newNode = TrieNode()
 					newNode.isEntry = true
-					children.put(tokens.head, newNode)
+					children.put(tokens.head.hashCode, newNode)
 				case trieNode => trieNode.isEntry = true
 			}
 		}
 		else {
-			children.get(tokens.head) match {
+			children.get(tokens.head.hashCode) match {
 				case null =>
 					val newNode = TrieNode()
 					newNode.add(tokens.tail)
-					children.put(tokens.head, newNode)
+					children.put(tokens.head.hashCode, newNode)
 				case trieNode =>
 					trieNode.add(tokens.tail)
 			}
@@ -57,7 +60,7 @@ case class TrieNode() {
 		else if (children == null)
 			false
 		else {
-			children.get(tokens.head) match {
+			children.get(tokens.head.hashCode) match {
 				case null => false
 				case trieNode => trieNode.contains(tokens.tail)
 			}
