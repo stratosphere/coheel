5f2ed8940af3c18f3fd63840a6ec6cf1f9345fa9
Stefan Bunk <stefan.bunk@googlemail.com>
2014-10-10 13:09:01 +0200

    Full code for the ROC curve.
    Former-commit-id: d5876eaf79f37e469796791a448b54f5589599f1

--- src/main/scala/de/uni_potsdam/hpi/coheel/programs/NerRocCurveProgram.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/programs/NerRocCurveProgram.scala
@@ -17,54 +17,64 @@ class NerRocCurveProgram extends Program with ProgramDescription with Logging {
 	override def getDescription = "Determining the ROC curve for the NER threshold."
 	override def getPlan(args: String*): Plan = {
 
-		val thresholds = ProgramHelper.filterNormalPages(ProgramHelper.getWikiPages(10)).flatMap { wikiPage =>
-			println(s"Reading ${wikiPage.pageTitle}.")
+		val thresholds = ProgramHelper.filterNormalPages(ProgramHelper.getWikiPages(15)).flatMap { wikiPage =>
 			// introduce all thresholds for each wikipage
-			(0.5 to 0.5 by 0.05).map { threshold =>
+			(0.00 to 0.95 by 0.05).map { threshold =>
 				(threshold, wikiPage)
 			}.toIterator
 		// group all computations for one threshold together, so that the trie needs to be built only once
 		}.groupBy { case (threshold, wikiPage) => threshold }
 
 
-		val rocValues = thresholds.reduceGroup { case wikiPages =>
-			println("Reducing a group.")
-			var trie: Trie = null
+		val rocValues = thresholds.reduceGroup { case wikiPagesIt =>
+
+			val wikiPages = wikiPagesIt.toList
+
+			val threshold = wikiPages.head._1
+			println(s"Working on threshold $threshold.")
+			TrieBuilder.buildThresholdTrie(threshold)
+			val thresholdTrie: Trie = TrieBuilder.thresholdTrie
 
 			var tp = 0
 			var tn = 0
 			var fp = 0
 			var fn = 0
-			wikiPages.map { case (threshold, wikiPage) =>
-				if (trie == null) {
-					// build threshold trie on first entry
-					TrieBuilder.buildThresholdTrie(threshold)
-					trie = TrieBuilder.thresholdTrie
-				}
+			wikiPages.foreach { case (_, wikiPage) =>
 				// determine the actual surfaces, from the real wikipedia article
 				val actualSurfaces = wikiPage.links.map { link => link.text }.toSet
-				println(wikiPage.pageTitle)
-				println(actualSurfaces)
+//				log.warn(wikiPage.pageTitle)
 
 				// determine potential surfaces, i.e. the surfaces that the NER would return for the current
 				// threshold
-				val potentialSurfaces = determinePotentialSurfaces(wikiPage)
-
-				(threshold, actualSurfaces.size, potentialSurfaces.size)
+				val potentialSurfaces = determinePotentialSurfaces(wikiPage, thresholdTrie)
+				// determine the potential surfaces for a trie with all surfaces, i.e. the threshold is
+				// zero
+				val fullPotentialSurfaces = determinePotentialSurfaces(wikiPage, TrieBuilder.fullTrie)
+				// TPs are those surfaces, which are actually in the text and our system would return it (for the
+				// current trie/threshold)
+				tp += actualSurfaces.intersect(potentialSurfaces).size
+				// TNs are those surfaces, which would be returned for threshold zero, but aren't because of the
+				// current threshold, and are no actual surfaces.
+				tn += fullPotentialSurfaces.diff(potentialSurfaces).diff(actualSurfaces).size
+				// FPs are those surfaces, which are returned but are not actually surfaces
+				fp += potentialSurfaces.diff(actualSurfaces).size
+				// FN are those surfaces, which are actual surfaces, but are not returned
+				fn += actualSurfaces.diff(potentialSurfaces).size
 			}
+			(threshold, tp, tn, fp, fn, tp.toDouble / (tp + fn), fp.toDouble / (fp + tn))
 		}.name("ROC-Curve-Values")
 
 		val rocValuesOutput = rocValues.write(nerRocCurvePath, CsvOutputFormat("\n", "\t"))
 		new ScalaPlan(Seq(rocValuesOutput))
 	}
 
-	def determinePotentialSurfaces(wikiPage: WikiPage): Set[String] =  {
+	def determinePotentialSurfaces(wikiPage: WikiPage, trie: Trie): Set[String] =  {
 		var potentialSurfaces = Set[String]()
 
 		val tokens = TokenizerHelper.tokenizeWithPositions(wikiPage.plainText).toArray
 
 		for (i <- 0 until tokens.size) {
-			val resultTokens = TrieBuilder.fullTrie.slidingContains[Token](tokens, { token: Token => token.word }, i)
+			val resultTokens = trie.slidingContains[Token](tokens, { token: Token => token.word }, i)
 			potentialSurfaces ++= resultTokens.map { containment =>
 				// extract the real world surfaces, by taking the whole string from the start of the first
 				// part to the end of the last part
