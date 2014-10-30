e954e801361eddd3dbe72f982f4d95849ef20ae2
Stefan Bunk <stefan.bunk@googlemail.com>
2014-10-30 16:36:09 +0100

    Introduce fake root element for xml, so that XmlReader can handle files with two separate roots.
    Former-commit-id: 5c8d6cf9153d03968a33a48d0f7b5a125b544c3e

--- src/main/scala/de/uni_potsdam/hpi/coheel/programs/ProgramHelper.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/programs/ProgramHelper.scala
@@ -37,7 +37,7 @@ object ProgramHelper {
 		val input = env.readTextFile(wikipediaFilesPath)
 
 		input.mapPartition { linesIt =>
-			val fileContent = linesIt.mkString(" ")
+			val fileContent = "<foo>" + linesIt.mkString("\n") + "</foo>"
 			val reader = new StringReader(fileContent)
 			val wikiPages = new WikiPageReader().xmlToWikiPages(reader)
 			val filteredWikiPages = wikiPages.filter { page =>
