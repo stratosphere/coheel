321c61c81423d471e153b10b05071e58cf2af537
Stefan Bunk <stefan.bunk@googlemail.com>
2014-11-01 17:27:14 +0100

    Throw count error to debug mapPartition size.
    Former-commit-id: 0ac5577d50e2b8af3de4a8fa35bbdfb02f9006e5

--- src/main/scala/de/uni_potsdam/hpi/coheel/programs/ProgramHelper.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/programs/ProgramHelper.scala
@@ -37,6 +37,7 @@ object ProgramHelper {
 		val input = env.readTextFile(wikipediaFilesPath)
 
 		input.mapPartition { linesIt =>
+			throw new RuntimeException(s"Error ${linesIt.size}")
 			val fileContent = "<foo>" + linesIt.mkString("\n") + "</foo>"
 			val reader = new StringReader(fileContent)
 			val wikiPages = new WikiPageReader().xmlToWikiPages(reader)
