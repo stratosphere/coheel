16a2f482bd1476f44b45c2dddb1f22e06fc1205f
Stefan Bunk <stefan.bunk@googlemail.com>
2015-01-07 18:00:10 +0100

    PerformanceTimer refactoring.
    Former-commit-id: 2e2d1f2ccdf23e2ec4e94409bebdd3528925e41d

--- src/main/scala/de/uni_potsdam/hpi/coheel/PerformanceTimer.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/PerformanceTimer.scala
@@ -5,11 +5,11 @@ import org.apache.log4j.Logger
 import scala.collection.immutable.ListMap
 
 object PerformanceTimer {
-
-	val logger = Logger.getLogger(getClass)
-
 	// order preserving map structure
 	var timers = ListMap[String, (Long, Long)]()
+
+	def startTime(event: String): Unit = startTimeFirst(event)
+	def endTime(event: String): Long   = endTimeFirst(event)
 
 	def startTimeFirst(event: String): Unit = {
 		timers.get(event) match {
@@ -23,41 +23,34 @@ object PerformanceTimer {
 		timers += (event -> (getTimeInMs, 0))
 	}
 
-	def endTimeFirst(event: String): Unit = {
+	def endTimeFirst(event: String): Long = {
 		timers.get(event) match {
 			case None =>
-				logger.warn("Timer not started.")
+				throw new Exception("Timer not started.")
 			case Some((start, end)) =>
 				if (end == 0) {
 					val newEnd = getTimeInMs
 					timers += (event ->(start, newEnd))
-				}
-				// else ignore, because the event already finished
+					newEnd - start
+				} else
+					throw new Exception("Timer already finished.")
 		}
 	}
-	def endTimeLast(event: String): Unit = {
+	def endTimeLast(event: String): Long = {
 		timers.get(event) match {
 			case None =>
-				logger.warn("Timer not started.")
+				throw new Exception("Timer not started.")
 			case Some((start, end)) =>
 				val newEnd = getTimeInMs
 				timers += (event ->(start, newEnd))
+				newEnd - start
 		}
 	}
 
-	private def print(event: String, start: Long, end: Long): Unit = {
-		val duration = end - start
-//		List((1000L, "us"), (1000L, "ms"), (1000L, "s"), (60L, "min")).fold(duration, "ns") { (acc, n) =>
-//			if (acc._1 < n._1) {
-//				()
-//			}
-//		}
-		println(f"$event%50s took ${end - start}%10s ms.")
-	}
-
 	def printTimerEvents(): Unit = {
+		val maxLength = timers.map(_._1.size).max
 		timers.foreach { case (event, (start, end)) =>
-			print(event, start, end)
+			println(f"${event.padTo(maxLength, ' ')} took ${end - start}%10s ms.")
 		}
 	}
 
