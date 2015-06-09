a34a7d07a067656be8b08ca4d3aa2dc560cefffb
Stefan Bunk <stefan.bunk@googlemail.com>
2015-06-09 14:02:29 +0200

    Fix bug.
    Former-commit-id: 0b834930f8dce9f145173f498a576a5e8916e0de

--- src/main/scala/de/uni_potsdam/hpi/coheel/programs/CoheelProgram.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/programs/CoheelProgram.scala
@@ -203,7 +203,7 @@ abstract class CoheelProgram[T]() extends ProgramDescription {
 		environment.readTextFile(languageModelsPath).map { line =>
 			val lineSplit = line.split('\t')
 			val pageTitle = lineSplit(0)
-			if (lineSplit.length <= 2) {
+			if (lineSplit.length < 2) {
 				throw new RuntimeException(s"$pageTitle not long enough: $line")
 			}
 			val model = lineSplit(1).split(' ').map { entrySplit =>
