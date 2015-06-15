c8dfe940d980c6c0a0cee5f421bf55f021bbb34b
Stefan Bunk <stefan.bunk@googlemail.com>
2015-06-15 13:10:10 +0200

    Update prepare scores for new data.
    Former-commit-id: 81776892ef3e184c2a4f56433f26a3382fc01e22

--- cluster-output/prepare-scores.scala
+++ cluster-output/prepare-scores.scala
@@ -4,20 +4,20 @@ import java.io.{File, PrintWriter}
 
 import scala.io.Source
 
-val reader = Source.fromFile(new File("scores.wiki"))
-val writer = new PrintWriter(new File("raw-scores.tsv"))
+val reader = Source.fromFile(new File("training-data.wiki"))
+val writer = new PrintWriter(new File("raw-training-data.tsv"))
 
-writer.println(Array("id", "prom", "promRank", "promDeltaTop", "promDeltaSucc",
+writer.println(Array("id", "NN", "NNP", "JJ", "VB", "CD", "SYM", "W", "prom", "promRank", "promDeltaTop", "promDeltaSucc",
 	"context", "contextRank", "contextDeltaTop", "contextDeltaSucc", "class").mkString("\t"))
 
 reader.getLines().foreach { line =>
 	val values = line.split('\t').toSeq
-	assert(values.length == 13)
+	assert(values.length == 20)
 	val ID_INDEX = 0
 	val SOURCE_INDEX = 2
 	val id = (values(ID_INDEX) + values(SOURCE_INDEX)).hashCode
 
-	val newValues = id.toString +: values.slice(4, 12) :+ (if (values.last == "true") "1.0" else "0.0")
+	val newValues = id.toString +: values.slice(4, values.size - 1) :+ (if (values.last == "true") "1.0" else "0.0")
 	val newLine = newValues.mkString("\t")
 	writer.println(newLine)
 }
