e960b457aeb24d1f19de6ee925a082d77bc9b902
Stefan Bunk <stefan.bunk@googlemail.com>
2015-11-18 21:00:39 +0100

    Specify charset in input format.

--- src/main/scala/de/uni_potsdam/hpi/coheel/io/WikiPageInputFormat.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/io/WikiPageInputFormat.scala
@@ -1,14 +1,15 @@
 package de.uni_potsdam.hpi.coheel.io
 
-import de.uni_potsdam.hpi.coheel.wiki.WikiPage
-import org.apache.flink.api.common.io.{DelimitedInputFormat, FileInputFormat}
+import java.nio.charset.StandardCharsets
+
+import org.apache.flink.api.common.io.DelimitedInputFormat
 
 //class WikiPageInputFormat extends FileInputFormat[String] {
 class WikiPageInputFormat extends DelimitedInputFormat[String] {
 
 	setDelimiter("<MARKER />")
 	override def readRecord(reuse: String, bytes: Array[Byte], offset: Int, numBytes: Int): String = {
-		new String(bytes, offset, numBytes)
+		new String(bytes, offset, numBytes, StandardCharsets.UTF_8)
 	}
 
 }
