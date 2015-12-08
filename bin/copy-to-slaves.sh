04c802a6da7692f470167237fb0c660519512ebc
Stefan Bunk <stefan.bunk@googlemail.com>
2015-12-08 16:43:45 +0100

    Add copy to slave script

---
+++ bin/copy-to-slaves.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+scp $1 hadoop10@tenem11:~/data/coheel &
+scp $1 hadoop10@tenem12:~/data/coheel &
+scp $1 hadoop10@tenem13:~/data/coheel &
+scp $1 hadoop10@tenem14:~/data/coheel &
+scp $1 hadoop10@tenem15:~/data/coheel &
+scp $1 hadoop10@tenem16:~/data/coheel &
+scp $1 hadoop10@tenem17:~/data/coheel &
+scp $1 hadoop10@tenem18:~/data/coheel &
+scp $1 hadoop10@tenem19:~/data/coheel &
+scp $1 hadoop10@tenem20:~/data/coheel &
+
