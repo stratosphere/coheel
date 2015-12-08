f05123ac38619194e9d32b4e3a1a9d2505a54e4c
Stefan Bunk <stefan.bunk@googlemail.com>
2015-12-08 16:27:45 +0100

    Remove old bins.

--- bin/transfer-wikidata.sh
+++
@@ -1,4 +0,0 @@
-mkdir stefan-wikidata
-cd stefan-wikidata
-scp "isfet:/data/wikipedia/*.dump" .
-hadoop dfs -copyFromLocal *.dump hdfs://tenemhead2/user/stefan.bunk/
--- bin/uniqueness.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cat $1 | grep -oh -P '^.*?(?=\t)' | sort | uniq > $1.unique
-wc -l $1.unique
