9da3dee6eda426b8902270e6775936e3641ecf6b
tongr <tongr@online>
2015-12-21 16:40:45 +0100

    verbose config output

--- bin/load-aws-config.sh
+++ bin/load-aws-config.sh
@@ -2,5 +2,5 @@
 export HDFS_NAME_NODE="$(curl http://169.254.169.254/latest/meta-data/public-hostname):8020"
 export FLINK_JOB_MANAGER=$(curl "$(yarn application -list |  grep RUNNING | grep Flink | cut -f9)/jobmanager/config" | jd -r 'from_entries | ."jobmanager.rpc.address"')
 export FLINK_JOB_MANAGER_PORT=$(curl "$(yarn application -list |  grep RUNNING | grep Flink | cut -f9)/jobmanager/config" | jd -r 'from_entries | ."jobmanager.rpc.port"')
-echo -e "cluster setup:\n hdfs: $HDFS_NAME_NODE\n flink: $FLINK_JOB_MANAGER:$FLINK_JOB_MANAGER_PORT"
+echo -e "cluster setup:\n HDFS NameNode: $HDFS_NAME_NODE\n Flink JobManager: $FLINK_JOB_MANAGER:$FLINK_JOB_MANAGER_PORT"
 
