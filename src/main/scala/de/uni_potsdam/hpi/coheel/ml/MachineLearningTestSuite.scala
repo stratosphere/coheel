2074150ff2640affbedfcc1720fa64a0b82dada6
Stefan Bunk <stefan.bunk@googlemail.com>
2016-02-03 10:56:51 +0100

    Learning three classifiers in parallel in ml suite did not work out because of memory issues. Now use weka internals to use 10 threads while training

--- src/main/scala/de/uni_potsdam/hpi/coheel/ml/MachineLearningTestSuite.scala
+++ src/main/scala/de/uni_potsdam/hpi/coheel/ml/MachineLearningTestSuite.scala
@@ -115,7 +115,7 @@ object MachineLearningTestSuite {
 		val expectedIds = expected.map( _._1)
 
 		var i = 1
-		classifiers.par.foreach { case (name, classifier) =>
+		classifiers.foreach { case (name, classifier) =>
 			println(new java.util.Date)
 			log.info(s"Starting training with ${FreeMemory.get(true)} MB of RAM")
 			val runtimeTry = Try(Timer.timeFunction {
@@ -221,7 +221,7 @@ object MachineLearningTestSuite {
 //			new Logistic,
 //			() => new FastRandomForest,
 //			() => new J48
-			() => new RandomForest
+			() => { val r = new RandomForest; r.setNumExecutionSlots(10); r }
 //			new SimpleLogistic,
 //			simpleLogistic,
 //			new MultilayerPerceptron,
