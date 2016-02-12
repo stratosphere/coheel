fae6d71d265c72e832ce2b57a9539a24917405b8
Stefan Bunk <stefan.bunk@googlemail.com>
2016-02-12 11:02:12 +0100

    prepare-scripts can be parameterized with results folder

--- bin/prepare-surface-link-probs-program.sh
+++ bin/prepare-surface-link-probs-program.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+RESULTS_FOLDER=${1:-results}
+
 echo "This script is to be run after the wikipedia extraction and before the surface link probs program."
 echo "It downloads the surface document counts, concatenates them to two files 12345 and 678910."
 echo "These files are then uploaded to surface-document-counts-halfs.wiki and"
 echo "can then be used for populating tries."
+echo ""
+echo "Will work on the folder: ${RESULTS_FOLDER}. Abort in next five seconds if wrong."
+sleep 5
 
 echo "Downloading .."
-$HADOOP_HOME/bin/hdfs dfs -copyToLocal hdfs://tenemhead2/home/stefan.bunk/results/surface-document-counts.wiki
+$HADOOP_HOME/bin/hdfs dfs -copyToLocal hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-document-counts.wiki
 
 echo "Catting .."
 cat surface-document-counts.wiki/{1,2,3,4,5}  > 12345
@@ -19,12 +24,13 @@ ls -lisah 12345 678910
 wc -l 12345 678910
 
 echo "Uploading .."
-$HADOOP_HOME/bin/hdfs dfs -mkdir hdfs://tenemhead2/home/stefan.bunk/results/surface-document-counts-halfs.wiki
-$HADOOP_HOME/bin/hdfs dfs -copyFromLocal 12345  hdfs://tenemhead2/home/stefan.bunk/results/surface-document-counts-halfs.wiki/12345
-$HADOOP_HOME/bin/hdfs dfs -copyFromLocal 678910 hdfs://tenemhead2/home/stefan.bunk/results/surface-document-counts-halfs.wiki/678910
+$HADOOP_HOME/bin/hdfs dfs -rmr hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-document-counts-halfs.wiki
+$HADOOP_HOME/bin/hdfs dfs -mkdir hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-document-counts-halfs.wiki
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal 12345  hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-document-counts-halfs.wiki/12345
+$HADOOP_HOME/bin/hdfs dfs -copyFromLocal 678910 hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-document-counts-halfs.wiki/678910
 
 echo "Setting replication"
-$HADOOP_HOME/bin/hdfs dfs -setrep 3 hdfs://tenemhead2/home/stefan.bunk/results/surface-document-counts-halfs.wiki/
+$HADOOP_HOME/bin/hdfs dfs -setrep 3 hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-document-counts-halfs.wiki/
 
 echo "Removing local files"
 rm 12345
--- bin/prepare-tries.sh
+++ bin/prepare-tries.sh
@@ -2,13 +2,18 @@
 
 set -e
 
+RESULTS_FOLDER=${1:-results}
+
 echo "This script is to be run after the surface link program and before the training data program."
 echo "It downloads the surface links probs, concatenates them to two files 12345 and 678910."
 echo "These files can then be uploaded to the tenems."
 echo "We do this by uploading to tenemhead and then from there to the tenems, because its faster."
+echo ""
+echo "Will work on the folder: ${RESULTS_FOLDER}. Abort in next five seconds if wrong."
+sleep 5
 
 echo "Downloading .."
-$HADOOP_HOME/bin/hdfs dfs -copyToLocal hdfs://tenemhead2/home/stefan.bunk/results/surface-link-probs.wiki
+$HADOOP_HOME/bin/hdfs dfs -copyToLocal hdfs://tenemhead2/home/stefan.bunk/$RESULTS_FOLDER/surface-link-probs.wiki
 
 cat surface-link-probs.wiki/12345/* > 12345
 cat surface-link-probs.wiki/678910/* > 678910
